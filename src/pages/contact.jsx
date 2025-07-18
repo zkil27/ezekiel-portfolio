b1321107ad09a785e8025f5b5002990cebfa85bb
johne <johnescueta26@gmail.com>
2025-07-19 03:15:56 +0800

    Update contact header to "Contact haha"

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,7 +1,7 @@
 export function contact() {
     return (
         <>
-            <h1>Contact OURS</h1>
+            <h1>Contact haha</h1>
             <p>If you have any questions, feel free to reach out!</p>
         </>
     );
