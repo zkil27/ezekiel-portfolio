8dcb404715563cff34668dd1510f61ead8d621d6
johne <johnescueta26@gmail.com>
2025-08-04 01:25:32 +0800

    Update About page header to include additional characters in "Work in Progress"

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ export function About() {
     return (
         <>
             <div className="projects-page">
-                <h1>Work in Progress 😔 </h1>
+                <h1>Work in Progressssssssssssssssssss 😔 </h1>
                 <p>Apologies, this About page is currently under construction. I’m working on crafting a clear and meaningful story about my journey, so please check back soon.</p>
             </div>
         </>
